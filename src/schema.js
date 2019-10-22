d8f2a1bde92b80c7804f9a374888e845ed03cad1
Whekin <stanislavkalishin@gmail.com>
2019-10-22 12:55:03 +0300

    Initial Commit

---
+++ src/schema.js
@@ -0,0 +1,51 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+    type Query {
+        launches: [Launch]!
+        launch(id: ID!): Launch
+        me: User
+    }
+
+    type Launch {
+        id: ID!
+        site: String
+        mission: Mission
+        rocket: Rocket
+        isBooked: Boolean!
+    }
+
+    type Rocket {
+        id: ID!
+        name: String
+        type: String
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        trips: [Launch]!
+    }
+
+    type Mission {
+        name: String
+        missionPatch(size: PatchSize): String
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
+    
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchIds: ID!): TripUpdateResponse!
+        login(email: String): String # login token
+    }
+
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches: [Launch]
+    }
+`;
