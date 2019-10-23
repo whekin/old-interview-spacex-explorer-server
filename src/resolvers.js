e56ed6171d1bb9de9e22b9feaced09c0df089fe2
Whekin <stanislavkalishin@gmail.com>
2019-10-23 13:42:18 +0300

    Add login mutation

--- src/resolvers.js
+++ src/resolvers.js
@@ -23,6 +23,12 @@ export default {
     launch: (_, { id }, { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
   },
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) return Buffer.from(email).toString('base64');
+    },
+  },
   Mission: {
     missionPatch: (mission, { size } = { size: 'LARGE' }) => {
       return size === 'SMALL'
